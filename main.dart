c880b329cb4d65014d52e1a70352410bf650e9cd
adyfp24 <adyfp24@gmail.com>
2024-02-29 16:57:46 +0700

    feat(init): initial commit

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main(){
+  List<String> iniList = ['tugas', 'pertama', 'pbm'];
+  Map<String, dynamic> iniMap = {
+      'tugas' : 1,
+      'kesulitan' : 1,
+      'bismillah' : true,
+  };
+  print(iniList);
+  print(iniMap);
+}
